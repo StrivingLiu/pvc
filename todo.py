8428a84481be319ae739dfbb87715f31810138d9
zliu <31382609+StrivingLiu@users.noreply.github.com>
2019-10-14 15:49:13 +0800

    Update todo.py

--- todo.py
+++ todo.py
@@ -2,10 +2,12 @@
 1. 需要一个fs的预加载机制；
 2. mvc线程隔离，完成线程间的通信；
 3. 多进程间的通信以及数据共享问题(多logger: https://github.com/jruere/multiprocessing-logging)
-4。 定义 model返回数据的规范(json?);
+4. 定义 model返回数据的规范(json?);
 5. pyvpx库的下载及导入;
-6。开源库的使用(use cayman);
+6. 开源库的使用(use cayman);
 7. prompt的进一步封装(自动补全规范定义)；
-8。实现非交互模式功能;
+8. 实现非交互模式功能;
+9. 完成外部命令的框架及几个sample case;
+10. 命令的自动补全以及参数导入的设计.
 '''
 
